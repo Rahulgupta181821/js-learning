281da56238b371dfc33cfed49957b13f58ebdf89
Rahul Kumar Gupta <68483409+Rahulgupta181821@users.noreply.github.com>
2023-12-29 12:25:00 +0000

    dataType

---
+++ zero/dataType.js
@@ -0,0 +1,14 @@
+//number => 2 to power 53
+//bigInt
+//string =>""
+//boolean => true/false
+//null=> standalone value
+//undefined
+//symbole=>unique
+
+
+//object
+
+console.log(typeof "Rahul")//string
+console.log(typeof null)//object
+console.log(typeof undefined)//undefined
