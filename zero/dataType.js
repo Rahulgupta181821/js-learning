b9b78a87f6269a73518619c6ae73bd658f7412ac
Rahul Kumar Gupta <68483409+Rahulgupta181821@users.noreply.github.com>
2023-12-30 21:50:20 +0000

    stack and heap memory

--- zero/dataType.js
+++ zero/dataType.js
@@ -1,3 +1,6 @@
+//primative data type
+// 7 types: number,bigInt, string, boolean , null, undefined, symbole
+
 //number => 2 to power 53
 //bigInt
 //string =>""
@@ -6,9 +9,38 @@
 //undefined
 //symbole=>unique
 
+//********************************** */
+//Referenc(Non primative datatype)
+// Array, Objects, Functions
 
 //object
 
 console.log(typeof "Rahul")//string
 console.log(typeof null)//object
 console.log(typeof undefined)//undefined
+
+
+//****************************************************How to work memory */
+// Stack(primitive) and Heap(Non primitive type)
+
+// Stack: When we use Primitive data type, It gives a copy of variable.
+// Heap: It gives reference of the variable
+//example
+let myYoutubeName="Code with chai";
+let anotherName=myYoutubeName;
+console.log(myYoutubeName);//Code with chai
+console.log(anotherName);//Code with chai
+anotherName="Rahul";
+console.log(anotherName);//Rahul
+console.log(myYoutubeName);//Code with chai
+
+let userOne={
+    email:'rahul@gmail.com',
+    upi:"rahul@ybl"
+}
+let userTwo=userOne;
+console.log(userTwo)//{ email: 'rahul@gmail.com', upi: 'rahul@ybl' }
+console.log(userOne)//{ email: 'rahul@gmail.com', upi: 'rahul@ybl' }
+userTwo.upi='3878883@ybl'
+console.log(userTwo)//{ email: 'rahul@gmail.com', upi: '3878883@ybl' }
+console.log(userOne)//{ email: 'rahul@gmail.com', upi: '3878883@ybl' }
