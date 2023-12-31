927c0c27bee49bc1feb82ecf7c1f6a7288186fb2
Rahul Kumar Gupta <68483409+Rahulgupta181821@users.noreply.github.com>
2023-12-31 22:23:11 +0000

    Arrays method

---
+++ basic/arrays2.js
@@ -0,0 +1,43 @@
+const marvel_heros = ["thor", "Ironman", "spiderman"]
+const dc_heros = ["superman", "flash", "batman"]
+
+// marvel_heros.push(dc_heros)
+// console.log(marvel_heros)
+// [ 'thor', 'Ironman', 'spiderman', [ 'superman', 'flash', 'batman' ] ]
+// console.log(marvel_heros[3][1])//flash
+
+
+// const new_marvel_heroes = marvel_heros.concat(dc_heros)
+// console.log(new_marvel_heroes)
+// //[ 'thor', 'Ironman', 'spiderman', 'superman', 'flash', 'batman' ]
+
+
+//spread method
+const all_new_heroes =[...marvel_heros, ...dc_heros]
+console.log(all_new_heroes)
+//[ 'thor', 'Ironman', 'spiderman', 'superman', 'flash', 'batman' ]
+
+
+const another_array = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
+const real_another_array = another_array.flat(Infinity)
+console.log(real_another_array)
+/*
+[
+    1, 2, 3, 4, 5,
+    6, 7, 6, 7, 4,
+    5
+ ]*/
+
+ console.log(Array.isArray("Hitesh"))
+ console.log(Array.from("Hitesh"))
+//  false
+// [ 'H', 'i', 't', 'e', 's', 'h' ]
+ console.log(Array.from({name: "hitesh"}))// interesting
+ //[] it does not convert into string because we don't give the instraction like key or values.
+
+
+ let score1=100
+ let score2=200
+ let score3=300
+ console.log(Array.of(score1,score2,score3))
+ //[100,200,300]
