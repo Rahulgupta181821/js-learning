5b42ee16a028d32b616a8cfc711fa245e455d68c
Rahul Kumar Gupta <68483409+Rahulgupta181821@users.noreply.github.com>
2024-01-03 16:38:14 +0000

    nested scope

--- basic2/02scope.js
+++ basic2/02scope.js
@@ -4,7 +4,7 @@
 // console.log(a)
 // console.log(b)
 // console.log(c)
-//**************************Scope************ */
+//**************************Scope=> {} it is know as scope.************ */
 // var c=400
 // if (true){
 //     let a=10
@@ -17,12 +17,87 @@
 // console.log(c)//30 it can access out of the given scope
 
 // ***********************2nd*****************
-let a =500
-if (true){
-    let a=10
-    const b=20
-    console.log("Inner:",a)//Inner: 10
+// let a =500
+// if (true){
+//     let a=10
+//     const b=20
+//     console.log("Inner:",a)//Inner: 10
+
+// }
+// console.log(a)//500
+// console.log(b)//error: b is not defined
+
+// *****************nested scope*************
+
+// function one(){
+//     const username="hitesh"
+
+//     function two(){
+//         const website = "youtube"
+//         console.log(username)
+//     }
+//     // console.log(website)// give error
+//     two()
+// }
+// one()
+
+// ***********Interesting***************
+// functions
+// function addone(num){
+//     return num +1
+
+// }
+// console.log(addone(5))//6
+
+// //expression
+
+// const addTwo = function(num){
+//     return num +2
+// }
+// addTwo(5)
+
+// ####################2nd######################
+
+// functions
+// console.log(addone(5))//6
+// function addone(num){
+//     return num +1
+
+// }
+
+
+// //expression
+
+// const addTwo = function(num){
+//     return num +2
+// }
+// addTwo(5)
+// ##############3rd####################
+// console.log(addone(5))//6
+// function addone(num){
+//     return num +1
+
+// }
+
+
+// //expression
+
+// const addTwo = function(num){
+//     return num +2
+// }
+// console.log(addTwo(5))//7
+
+// ####################4th#######################
+
+console.log(addone(5))//6
+function addone(num){
+    return num +1
 
 }
-console.log(a)//500
-// console.log(b)//error: b is not defined
+
+
+//expression
+console.log(addTwo(5))//Cannot access 'addTwo' before initialization
+const addTwo = function(num){
+    return num +2
+}
