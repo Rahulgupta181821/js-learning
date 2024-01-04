b2b5b85c68f1aefabe4fb37f78da58c172cedcb5
Rahul Kumar Gupta <68483409+Rahulgupta181821@users.noreply.github.com>
2024-01-04 23:04:01 +0000

    iteration

---
+++ 04ControlFlow/iterations.js
@@ -0,0 +1,55 @@
+// for of loop
+// ["","",""]
+// [{},{},{}]
+
+// const arr =[1,2,3,4,5]
+// for (const num of arr) {
+//     console.log(num)
+// }
+
+// const greetings ="Hello world!"
+// for (const greet of greetings) {
+//     console.log(greet)
+// }
+
+/*
+//Maps
+const map1 = new Map();
+
+map1.set('a', 1);
+map1.set('b', 2);
+map1.set('c', 3);
+
+console.log(map1.get('a'));
+// Expected output: 1
+
+map1.set('a', 97);
+
+console.log(map1.get('a'));
+// Expected output: 97
+
+console.log(map1.size);
+// Expected output: 3
+
+map1.delete('b');
+
+console.log(map1.size);
+// Expected output: 2
+
+for (const key of map1) {
+    console.log(key)
+}
+
+for (const [key,value] of map1) {
+    console.log("key:",key)
+    console.log("value:",value)
+}
+*/
+
+// const myObject ={
+//     'game1':'NFS',
+//     'game2': 'Spiderman'
+// }
+// for (const [key,value] of myObject) {
+//     console.log(key,":",value)
+// }//myObject is not iterable
