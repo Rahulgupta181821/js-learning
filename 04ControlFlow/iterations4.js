f510cfe350929b568e73a3b38f1b2ee04a9e7520
Rahul Kumar Gupta <68483409+Rahulgupta181821@users.noreply.github.com>
2024-01-05 01:21:19 +0000

    map and filter

---
+++ 04ControlFlow/iterations4.js
@@ -0,0 +1,32 @@
+const myNumber = [1,2,3,4,5,6,7,8,9]
+// const newNums = myNumber.map( (num) => num + 10)
+// console.log(newNums)
+/*
+[
+  11, 12, 13, 14, 15,
+  16, 17, 18, 19
+]
+*/
+
+//chaning
+// const newNums= myNumber
+//             .map( (num) => num*10)
+//             .map( (num) => num+1)
+// console.log(newNums)
+/* 
+[
+  11, 21, 31, 41, 51,
+  61, 71, 81, 91
+]
+
+*/
+
+
+const newNums= myNumber
+            .map( (num) => num*10)
+            .map( (num) => num+1)
+            .filter( (num) => num>=40)
+console.log(newNums)
+/* 
+[ 41, 51, 61, 71, 81, 91 ]
+*/
